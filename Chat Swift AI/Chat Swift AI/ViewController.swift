19ba5335e056e7c279ed032753038e42bad66830
Jef-in <jefinabduljaleel@gmail.com>
2023-01-19 12:14:33 +0530

    initial project created

---
+++ Chat Swift AI/Chat Swift AI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat Swift AI
+//
+//  Created by Jefin on 19/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
